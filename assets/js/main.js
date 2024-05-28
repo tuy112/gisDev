a12cc87049281f01e6eaee81899cfb25f450d2d3
KimJaeHyeok <master_catch@naver.com>
2024-05-28 09:44:13 +0900

    fix: 배경지도 띄우기 성공

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
-let map = new ol.Map({
-    target: 'map',
-    layers: [
+function mapInit() {
+  let map = new ol.Map({
+      target: 'map',
+      layers: [
       new ol.layer.Tile({
-        source: new ol.source.OSM({
+          source: new ol.source.OSM({
           url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
-        })
+          })
       })
-    ],
-    view: new ol.View({
+      ],
+      view: new ol.View({
+      // center: ol.proj.fromLonLat([128.4, 35.7]),
       center: ol.proj.fromLonLat([128.4, 35.7]),
-      zoom: 7
-    })
+      zoom: 8
+      })
   });
+}
+
+// 함수 출력
+$(document).ready(function(){
+  mapInit();
+})
